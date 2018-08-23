cf6365bfa87f355345f56f05fcbf58928557e769
hongyuw <39173865+hongyuw@users.noreply.github.com>
2018-08-23 13:27:56 +0800

    Update input.ino

--- input.ino
+++ input.ino
@@ -1,35 +1,39 @@
+/*
+  This file reads six motor position information from the robotic arm, and map the readings to our desired intervals
+*/
 #include "DualVNH5019MotorShield.h"
-DualVNH5019MotorShield md;
+DualVNH5019MotorShield md; //motor driver
 
-const int switchA = 2, switchB = 3, switchC = 4, switchD = 5, switchTrigger = 6;
-const int xAxis = A0, yAxis = A1, zAxis = A2, Pitch = A3, Roll = A4, Yaw = A5;
+const int switchA = 2, switchB = 3, switchC = 4, switchD = 5, switchTrigger = 6;  //trigger and 4 buttons on the robotic arm
+const int xAxis = A0, yAxis = A1, zAxis = A2, Pitch = A3, Roll = A4, Yaw = A5;   //six potentiometers connected to the motors
+//initiate the mapped variables
 int newxAxis = 0, newyAxis = 0, newzAxis = 0, newPitch = 0, newRoll = 0, newYaw = 0;
-int Force = 0;
-int Trigger = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   md.init();
+  
+  //we only use X,Y,Z,Pitch  A0--X, A1--Y, A2--Z, A3--Pitch
   pinMode(A0,INPUT);
   pinMode(A1,INPUT);
   pinMode(A2,INPUT);
-  pinMode(A3,INPUT); // 3
+  pinMode(A3,INPUT); 
 }
-//A0--X,A3--Pitch
+
 
 void loop() {
   serialprint1();
 }
 
 void serialprint1() {
+  //map the readings to desired intervals
   newxAxis = map(analogRead(xAxis),741,951,200,300); 
   newyAxis = map(analogRead(yAxis),100,200,100,200); 
   newzAxis = map(analogRead(zAxis),0,133,200,300);
   newPitch = map(analogRead(Pitch),920,890,200,300);
-  Trigger = digitalRead(6);
-  // Force = 4 * newxAxis;
-  //md.setM1Speed(Force);
+  
+  //formatted output
   Serial.print(newxAxis+200);
   Serial.print(",");
   Serial.print(newyAxis-200);
@@ -37,10 +41,7 @@ void serialprint1() {
   Serial.print(newzAxis);
   Serial.print(",");
   Serial.println(newPitch);
- // Serial.print(newxAxis); // A0
- // Serial.print(",");
- // Serial.println(md.getM1CurrentMilliamps());
- // Serial.println(newPitch); // A3
+
   Serial.flush();
   delay(10);
 }
